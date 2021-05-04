6d678bac1829dde3b705dcb5bc1d2b7a2da7e4c6
zeyad <zeyad@zeyads-mbp>
2021-05-04 23:05:28 +0400

    Commit Test

--- src/myPackage/Driver.java
+++ src/myPackage/Driver.java
@@ -1,5 +1,58 @@
 package myPackage;
+import java.util.*;
+
+
+class Process {
+
+	int pid;
+	boolean type;
+	int base;
+	int limit;
+
+	public Process(int pid, boolean type, int base, int limit) {
+		super();
+		this.pid = pid;
+		this.type = type;
+		this.base = base;
+		this.limit = limit;
+	}
+
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public boolean isType() {
+		return type;
+	}
+	public void setType(boolean type) {
+		this.type = type;
+	}
+	public int getBase() {
+		return base;
+	}
+	public void setBase(int base) {
+		this.base = base;
+	}
+	public int getLimit() {
+		return limit;
+	}
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}
+
+
+
+}
 
 public class Driver {
-	
+	public int nextFitPtr=0;
+	public static void main(String[] args) {	
+		LinkedList<Process> memory = new LinkedList<Process>();
+		LinkedList<Process> processTable = new LinkedList<Process>();
+		Process test = new Process(1000,true,0,10);
+		memory.add(test);
+		processTable.add(test);
+	}
 }
